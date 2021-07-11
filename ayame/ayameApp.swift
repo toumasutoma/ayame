de20e482dfdbb280db3aa5a6c8254b53bcbf9180
toumasutoma <thomas.chiron@zf.com>
2021-07-11 15:15:54 +0200

    Initial Commit

---
+++ ayame/ayameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ayameApp.swift
+//  ayame
+//
+//  Created by thomas on 11.07.21.
+//
+
+import SwiftUI
+
+@main
+struct ayameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
